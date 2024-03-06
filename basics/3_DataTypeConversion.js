8035dda9ebb7584df4af94627fc8c46701a3050e
Ansh <139133474+Clutches9167@users.noreply.github.com>
2024-03-06 03:37:16 +0000

    coversion Number Datatypes

---
+++ basics/3_DataTypeConversion.js
@@ -0,0 +1,20 @@
+// let score = 69
+// let score = '234' // number after Conversion
+
+// let score = '234abc' // NaN ( ( Not a Number ) after Conversion
+// let score = null // 0 after number  Conversion
+// let score = undefined // NaN after Conversion
+// let score = true // 1 after number  Conversion
+let score = false // 0 after number  Conversion
+
+// console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let loggedIn = ""  // 0 -> false,  1-> true, ""-> false ( bcz 0 string hai ), "Ansh" -> true . " " -> true (kyu ki space count hoga )
+let booleanConverison = Boolean(loggedIn)
+console.log(booleanConverison);
