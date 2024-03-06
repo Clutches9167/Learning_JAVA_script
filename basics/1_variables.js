321600bf1a7d03b08eca3e53b68b2ee4f1cc0608
Ansh <139133474+Clutches9167@users.noreply.github.com>
2024-03-06 03:02:28 +0000

    variables

---
+++ basics/1_variables.js
@@ -0,0 +1,17 @@
+const customerId = 12345   // value is Lock kabhi change nahi ho sakhti yeah value abh //
+let emailAdd = 'Asdfffufj@gmail.com'  // let for variables //
+var name = 'Ansh'            // {  }  -> ise Scope kehte hai  yeah purane samay main Java Script padh nahi pata tha isliye agar kisi developer ne for ex:- if loop main value change kar di toh voh sarre jagah se change ho jati thi ise solve karne ke liye we use ' let '//
+let accountPass = '67289'                     
+customerCountry = "India"  // java script main bina let / var likh bhi variables declare kar sakhte hai 
+let acccountState = ""
+let accountAdd;    // it will give you undefined kyu ki value hi defined nahi ki hai 
+
+console.log([customerId, emailAdd, name, accountPass, customerCountry]);
+
+console.table([customerId, emailAdd, name, accountPass, customerCountry, acccountState, accountAdd]);
+
+// Investigation //
+
+// customerId = 4322
+// console.log(customerId);
+
