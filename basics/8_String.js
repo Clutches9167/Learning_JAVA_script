66a3cc4f8bd8d988e48e7c95b025f1eb8c5d1404
Ansh <139133474+Clutches9167@users.noreply.github.com>
2024-03-07 03:09:45 +0000

    Strings

--- basics/8_String.js
+++ basics/8_String.js
@@ -2,6 +2,7 @@ const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
+// String interpolation 
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
@@ -26,7 +27,7 @@ const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-// const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
